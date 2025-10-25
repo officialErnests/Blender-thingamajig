3e8f4ac5ac2287574f506e8828c970278630200a
n0o0b090lv <ernestspursw@gmail.com>
2025-10-25 20:44:14 +0300

    Wip materials ;-;

--- monaLisa.py
+++ monaLisa.py
@@ -8,8 +8,7 @@ with open('actionList.json', 'r') as file:
 
 input("Hover over blender and prepear to be blasted away ;))\nenter to start XDD\nalso hold q to stop ;DD")
 for keystroke in keystrokes['allOfThingies']:
-    if (keyboard.is_pressed('q')):
-        break
+    if (keyboard.is_pressed('q')): break
     match keystroke[0]:
         case 0:
             keyboard.press_and_release(keystroke[2])
@@ -22,12 +21,32 @@ for keystroke in keystrokes['allOfThingies']:
             mouse.click('right')
         case 4:
             for char in str(keystroke[2]):
-                if (char == '(' or
-                    char == ')' or 
-                    char == '_'):
-                    keyboard.press_and_release("shift+"+char)
-                else:
-                    keyboard.press_and_release(char)
+                if (keyboard.is_pressed('q')): break
+                match char:
+                    case '.':
+                        keyboard.press_and_release("dot")
+                    case '(':
+                        keyboard.press("shift")
+                        keyboard.press_and_release("9")
+                        keyboard.release("shift")
+                    case 'T':
+                        keyboard.press("shift")
+                        keyboard.press_and_release("t")
+                        keyboard.release("shift")
+                    case '_':
+                        keyboard.press("shift")
+                        keyboard.press_and_release("-")
+                        keyboard.release("shift")
+                    case ')':
+                        keyboard.press("shift")
+                        keyboard.press_and_release("0")
+                        keyboard.release("shift")
+                    case '_':
+                        keyboard.press("shift")
+                        keyboard.press_and_release("-")
+                        keyboard.release("shift")
+                    case _:
+                        keyboard.press_and_release(char)
                 time.sleep(float(keystroke[3]))
         case 5:
             mouse.move(keystroke[2],keystroke[3], absolute = keystroke[4])
